4b8220038f816e1713ddf6e61e91159f52d5037b
Jaroslav Zhidkov <jaropolk1104@gmail.com>
2023-10-17 09:53:29 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,7 +1,12 @@
 const button = document.querySelector('button');
+let videoDisplayed = false;
 
 button.addEventListener('click', function() {
-  const videoContainer = document.createElement('div');
-  videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
-  document.body.appendChild(videoContainer);
+  if (!videoDisplayed) {
+    const videoContainer = document.createElement('div');
+    videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+    document.body.appendChild(videoContainer);
+    
+    videoDisplayed = true;
+  }
 });
